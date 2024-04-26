0e4bfb3a46159f3add1d28d84deb9828ebce0f3b
Eat._.Noey <155715976+Jelliiiii@users.noreply.github.com>
2024-04-26 16:30:27 +0900

    Title: 세 정수형 변수 선언, Time: 104ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240426/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=7, b=23, c=30;
+        System.out.println(a + " + " + b  + " = " + c);
+        // 여기에 코드를 작성해주세요.
+    }
+}
