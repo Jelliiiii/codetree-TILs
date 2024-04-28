162c1717768a9dd83cbacc83fb6cc7d5d46f8a00
Eat._.Noey <155715976+Jelliiiii@users.noreply.github.com>
2024-04-28 13:26:16 +0900

    Title: 데이터 교환, Time: 85ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240428/데이터 교환/exchange-data.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5, b = 6, c = 7, d = b;
+        b = a;
+        a = c;
+        c = d;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        // 여기에 코드를 작성해주세요.
+    }
+}
