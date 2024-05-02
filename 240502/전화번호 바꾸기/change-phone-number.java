948fcae5517c3a569e9e764bae17b21612484cfd
Eat._.Noey <155715976+Jelliiiii@users.noreply.github.com>
2024-05-02 16:02:09 +0900

    Title: 전화번호 바꾸기, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240502/전화번호 바꾸기/change-phone-number.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("-");
+        String a = sc.next(), b = sc.next(), c = sc.next();
+        System.out.print(a + "-" + c + "-" + b);
+        sc.close();
+        // 여기에 코드를 작성해주세요.
+    }
+}
