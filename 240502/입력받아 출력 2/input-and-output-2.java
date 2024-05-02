36e977947ca0db4644a8ccb9506883fa60876e17
Eat._.Noey <155715976+Jelliiiii@users.noreply.github.com>
2024-05-02 11:53:49 +0900

    Title: 입력받아 출력 2, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240502/입력받아 출력 2/input-and-output-2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("-");
+        String s = sc.next(), t = sc.next();
+        System.out.print(s + t);
+        // 여기에 코드를 작성해주세요.
+    }
+}
