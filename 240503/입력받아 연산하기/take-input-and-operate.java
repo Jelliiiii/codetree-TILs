88af6b0cf548bd982740033208c2c887938148cc
Eat._.Noey <155715976+Jelliiiii@users.noreply.github.com>
2024-05-03 16:18:42 +0900

    Title: 입력받아 연산하기, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240503/입력받아 연산하기/take-input-and-operate.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt()+87;
+        int b = sc.nextInt()%10;
+        System.out.print((a + "\n" + b));
+        // 여기에 코드를 작성해주세요.
+    }
+}
