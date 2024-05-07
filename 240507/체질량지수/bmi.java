1d9b247000a0c199d19e08ab79bee32b294bf776
Eat._.Noey <155715976+Jelliiiii@users.noreply.github.com>
2024-05-07 21:08:02 +0900

    Title: 체질량지수, Time: 164ms, Memory: 10MB, Status: Passed - CodeTree

--- 240507/체질량지수/bmi.java
+++ 240507/체질량지수/bmi.java
@@ -6,9 +6,9 @@ public class Main {
         double b = (10000 * w)/(h * h);
         int bb = (int)Math.floor(b);
         if(bb>=25){
-            System.out.printf(bb + "\n" + "Obesity");
+             System.out.printf("%d\nObesity", bb);
         }else{
-            System.out.printf(bb);
+            System.out.printf("%d", bb);
         }
         sc.close();
         // 여기에 코드를 작성해주세요.
