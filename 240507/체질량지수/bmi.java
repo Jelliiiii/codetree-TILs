ee8867d7717413ec40b831aba2cf3dc816d783cd
Eat._.Noey <155715976+Jelliiiii@users.noreply.github.com>
2024-05-07 21:05:46 +0900

    Title: 체질량지수, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240507/체질량지수/bmi.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double h = sc.nextInt(), w = sc.nextInt();
+        double b = (10000 * w)/(h * h);
+        int bb = (int)Math.floor(b);
+        if(bb>=25){
+            System.out.printf(bb + "\n" + "Obesity");
+        }else{
+            System.out.printf(bb + "\n" + "Normal");
+        }
+        // 여기에 코드를 작성해주세요.
+    }
+}
