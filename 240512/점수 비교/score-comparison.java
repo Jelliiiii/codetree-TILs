01bb643855e4f21c644f828e8aa6b91f67a75b09
Eat._.Noey <155715976+Jelliiiii@users.noreply.github.com>
2024-05-12 23:05:28 +0900

    Title: 점수 비교, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240512/점수 비교/score-comparison.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt(), d = sc.nextInt();
+        if(a>c && b>d){
+            System.out.print(1);
+        }else{
+            System.out.print(0);
+        }
+        sc.close();
+        // 여기에 코드를 작성해주세요.
+    }
+}
