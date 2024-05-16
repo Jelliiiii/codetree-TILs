25cad9bc0d349fe4bb7c6f3906c72a56b5d05b0e
Eat._.Noey <155715976+Jelliiiii@users.noreply.github.com>
2024-05-16 14:56:29 +0900

    Title: 장학금, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240516/장학금/scholarship.java
+++ 240516/장학금/scholarship.java
@@ -4,12 +4,12 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int m = sc.nextInt(), f = sc.nextInt();
         int s = (m+f)/2;
-        if(s<90){
-            System.out.print(0);
-        }else if(s>=90){
-            System.out.print(50000);
-        }else{
+        if (s >= 95) {
             System.out.print(100000);
+        } else if (s >= 90) {
+            System.out.print(50000);
+        } else {
+            System.out.print(0);
         }
         sc.close();
         // 여기에 코드를 작성해주세요.
