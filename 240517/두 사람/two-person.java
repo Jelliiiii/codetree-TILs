56268271bd48e032d6778b46a28b50c5bd92bdb5
Eat._.Noey <155715976+Jelliiiii@users.noreply.github.com>
2024-05-17 22:27:12 +0900

    Title: 두 사람, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240517/두 사람/two-person.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        String b = sc.next();
+        int c = sc.nextInt();
+        String d = sc.next();
+        if(b.equals("W")  && d.equals("M")){
+            System.out.print(0);
+        }else if(a>=19 || c>=19){
+            System.out.print(1);
+        }
+        sc.close();
+        // 여기에 코드를 작성해주세요.
+    }
+}
