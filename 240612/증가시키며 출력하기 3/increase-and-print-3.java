2c9a281bca8342beeb4aa2f083f8c531415726ca
Eat._.Noey <155715976+Jelliiiii@users.noreply.github.com>
2024-06-12 23:31:45 +0900

    Title: 증가시키며 출력하기 3, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240612/증가시키며 출력하기 3/increase-and-print-3.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int i=10;
+        while(i<=26){
+            System.out.print(i + " ");
+            i+=2;
+        }
+        // 여기에 코드를 작성해주세요.
+    }
+}
