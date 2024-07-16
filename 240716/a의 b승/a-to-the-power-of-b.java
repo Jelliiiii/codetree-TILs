5231ccc0b7504094a7f8c6a296ee5b2c20654cca
Eat._.Noey <155715976+Jelliiiii@users.noreply.github.com>
2024-07-16 17:08:51 +0900

    Title: a의 b승, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

--- 240716/a의 b승/a-to-the-power-of-b.java
+++ 240716/a의 b승/a-to-the-power-of-b.java
@@ -4,9 +4,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int pro = a;
+        int pro = 1;
         for(int i=1;i<=b;i++){
-            pro+=a*i;
+            pro*=a;
         }
         System.out.print(pro);
         sc.close();
