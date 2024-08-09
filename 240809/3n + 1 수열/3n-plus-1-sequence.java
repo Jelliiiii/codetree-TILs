25c394f365cea7ad895f285395009c5792c43ed1
Eat._.Noey <155715976+Jelliiiii@users.noreply.github.com>
2024-08-09 18:34:54 +0900

    Title: 3n + 1 수열, Time: 159ms, Memory: 10MB, Status: Passed - CodeTree

--- 240809/3n + 1 수열/3n-plus-1-sequence.java
+++ 240809/3n + 1 수열/3n-plus-1-sequence.java
@@ -5,6 +5,10 @@ public class Main {
         int n = sc.nextInt();
         int count = 0;
         while(true){
+            if(n==1){
+                System.out.print(count);
+                break;
+            }
             if(n%2==0){
                 n = n/2;
                 count++;
@@ -12,10 +16,7 @@ public class Main {
                 n = (n*3) + 1;
                 count++;
             }
-            if(n==1){
-                System.out.print(count);
-                break;
-            }
+            
         }
         sc.close();
         // 여기에 코드를 작성해주세요.
