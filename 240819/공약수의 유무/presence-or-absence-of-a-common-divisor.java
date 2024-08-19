1a6f4df9108c6ef46f9cf3542dd39087ecc4945a
Eat._.Noey <155715976+Jelliiiii@users.noreply.github.com>
2024-08-19 12:34:07 +0900

    Title: 공약수의 유무, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 240819/공약수의 유무/presence-or-absence-of-a-common-divisor.java
+++ 240819/공약수의 유무/presence-or-absence-of-a-common-divisor.java
@@ -7,7 +7,7 @@ public class Main {
         int b = sc.nextInt();
         boolean oo = false;
         for(int i=a; i<=b; i++){
-            if(1920%i==0||2880%i==0){
+            if(1920%i==0&&2880%i==0){
                 oo = true;
                 break;
             }else{
