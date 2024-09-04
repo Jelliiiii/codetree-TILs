21db126b7b75ea49a71305b4dab55ee08312c56b
Eat._.Noey <155715976+Jelliiiii@users.noreply.github.com>
2024-09-04 10:20:03 +0900

    Title: 건너뛰고 별표 출력하기, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240904/건너뛰고 별표 출력하기/skip-and-print-a-star.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+            System.out.println();
+        }
+        for(int i = 1; i<=n-1; i++){
+            for(int j=n-1; j>=i; j--){
+                System.out.print("*");
+            }
+            System.out.println();
+            System.out.println();
+        }
+        sc.close();
+        // 여기에 코드를 작성해주세요.
+    }
+}
