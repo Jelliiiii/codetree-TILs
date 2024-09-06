8cd6424b8dfa48003a06157fbe216460d17eddd8
Eat._.Noey <155715976+Jelliiiii@users.noreply.github.com>
2024-09-06 14:50:05 +0900

    Title: 별표 출력하기 3, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240906/별표 출력하기 3/print-star-3.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < 2 * i; j++) {
+                System.out.print(" ");
+            }
+            
+
+            for (int j = 0; j < (2 * (n - i) - 1); j++) {
+                System.out.print("*");
+                if (j < (2 * (n - i) - 2)) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
