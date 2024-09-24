747e41c762b6f8a7dcc8cf719c7ae25490486898
Eat._.Noey <155715976+Jelliiiii@users.noreply.github.com>
2024-09-24 14:29:19 +0900

    Title: 별표 출력하기 12, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240924/별표 출력하기 12/print-star-12.java
+++ 240924/별표 출력하기 12/print-star-12.java
@@ -5,41 +5,36 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        if (n == 1) {
-            System.out.println("*");
-        } else {
-            // 총 n개의 줄 출력
-            for (int i = 0; i < n; i++) {
-                // 첫 번째 줄: n개의 별 출력
-                if (i == 0) {
-                    for (int j = 0; j < n; j++) {
-                        System.out.print("* ");
-                    }
-                } 
-                // 짝수 줄: 앞에 공백 추가, 별 사이에 공백 없이 별만 출력
-                else if (i % 2 == 0) {
-                    for (int j = 0; j < i; j++) {
-                        System.out.print(" ");  // 앞에 공백 추가
-                    }
-                    for (int j = 0; j < (n - i / 2); j++) {
-                        System.out.print("* ");
-                    }
-                }
-                // 홀수 줄: 앞에 공백 추가, 별 사이에 공백 추가하여 출력
-                else {
-                    for (int j = 0; j < i; j++) {
-                        System.out.print(" ");  // 앞에 공백 추가
-                    }
-                    for (int j = 0; j < (n - i / 2); j++) {
-                        if (j % 2 == 0) {
-                            System.out.print("* ");
-                        } else {
-                            System.out.print("  "); // 별 사이에 공백 추가
-                        }
-                    }
-                }
-                System.out.println();  // 한 줄 출력 후 줄 바꿈
+        // 첫 번째 줄: n개의 별 출력
+        for (int i = 0; i < n; i++) {
+            System.out.print("* ");
+        }
+        System.out.println(); // 첫 번째 줄 출력 완료 후 줄 바꿈
+
+        // 두 번째 줄부터 n개의 줄을 출력
+        for (int i = 1; i < n; i++) {
+            // 앞에 공백 추가
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
             }
+
+            // 짝수 줄에서는 별 사이에 공백 없이 출력
+            if (i % 2 == 0) {
+                for (int j = 0; j < (n - i / 2); j++) {
+                    System.out.print("* ");
+                }
+            }
+            // 홀수 줄에서는 별 사이에 공백을 추가하여 출력
+            else {
+                for (int j = 0; j < (n - i / 2); j++) {
+                    if (j % 2 == 0) {
+                        System.out.print("* ");
+                    } else {
+                        System.out.print("  "); // 별 사이에 공백 추가
+                    }
+                }
+            }
+            System.out.println(); // 한 줄 출력 후 줄 바꿈
         }
         sc.close();
     }
