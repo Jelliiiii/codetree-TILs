267fe6388ada966185fa40cc0830578a0730b742
Eat._.Noey <155715976+Jelliiiii@users.noreply.github.com>
2024-09-24 14:25:17 +0900

    Title: 별표 출력하기 12, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240924/별표 출력하기 12/print-star-12.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n == 1) {
+            System.out.println("*");
+        } else {
+            for (int i = 0; i < (n % 2 == 0 ? n : n - 1); i++) {
+                if (i == 0) {
+                    for (int j = 0; j < n; j++) {
+                        System.out.print("* ");
+                    }
+                } 
+                else if (i % 2 == 0) {
+                    for (int j = 0; j < i; j++) {
+                        System.out.print(" ");
+                    }
+                    for (int j = 0; j < (n - i / 2); j++) {
+                        System.out.print("* ");
+                    }
+                }
+                else {
+                    for (int j = 0; j < i; j++) {
+                        System.out.print(" ");
+                    }
+
+                    for (int j = 0; j < (n - i / 2); j++) {
+                        if (j % 2 == 0) {
+                            System.out.print("* ");
+                        } else {
+                            System.out.print("  ");
+                        }
+                    }
+                }
+                System.out.println(); 
+        }
+        sc.close();
+    }
+}
