84a916b9b6184f23ace5fdfff56a002ade7f5629
Eat._.Noey <155715976+Jelliiiii@users.noreply.github.com>
2024-09-24 14:28:00 +0900

    Title: 별표 출력하기 12, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240924/별표 출력하기 12/print-star-12.java
+++ 240924/별표 출력하기 12/print-star-12.java
@@ -8,33 +8,37 @@ public class Main {
         if (n == 1) {
             System.out.println("*");
         } else {
-            for (int i = 0; i < (n % 2 == 0 ? n : n - 1); i++) {
+            // 총 n개의 줄 출력
+            for (int i = 0; i < n; i++) {
+                // 첫 번째 줄: n개의 별 출력
                 if (i == 0) {
                     for (int j = 0; j < n; j++) {
                         System.out.print("* ");
                     }
                 } 
+                // 짝수 줄: 앞에 공백 추가, 별 사이에 공백 없이 별만 출력
                 else if (i % 2 == 0) {
                     for (int j = 0; j < i; j++) {
-                        System.out.print("  ");
+                        System.out.print(" ");  // 앞에 공백 추가
                     }
                     for (int j = 0; j < (n - i / 2); j++) {
                         System.out.print("* ");
                     }
                 }
+                // 홀수 줄: 앞에 공백 추가, 별 사이에 공백 추가하여 출력
                 else {
                     for (int j = 0; j < i; j++) {
-                        System.out.print("  ");
+                        System.out.print(" ");  // 앞에 공백 추가
                     }
                     for (int j = 0; j < (n - i / 2); j++) {
                         if (j % 2 == 0) {
                             System.out.print("* ");
                         } else {
-                            System.out.print("  ");
+                            System.out.print("  "); // 별 사이에 공백 추가
                         }
                     }
                 }
-                System.out.println(); 
+                System.out.println();  // 한 줄 출력 후 줄 바꿈
             }
         }
         sc.close();
