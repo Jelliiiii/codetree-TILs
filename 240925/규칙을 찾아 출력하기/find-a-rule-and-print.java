50c60b3a12c0a76514ee020b8025af215c3e7547
Eat._.Noey <155715976+Jelliiiii@users.noreply.github.com>
2024-09-25 13:40:06 +0900

    Title: 규칙을 찾아 출력하기, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 240925/규칙을 찾아 출력하기/find-a-rule-and-print.java
+++ 240925/규칙을 찾아 출력하기/find-a-rule-and-print.java
@@ -1,22 +1,24 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == 0 || i == n - 1 || j == 0 || j == n - 1) {
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==0||j==n-1){
                     System.out.print("* ");
-                    if (j < n - 1) System.out.print("  ");
-                } else {
+                }else if(i==j || i<=j){
                     System.out.print("  ");
-                    if (j < n - 1) System.out.print("  ");
+                }else{
+                    System.out.print("* ");
                 }
+            
             }
+
             System.out.println();
         }
-        sc.close();
     }
 }
