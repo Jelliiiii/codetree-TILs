b7f4e95f3d6f8eb6a7537c3681561bb34d6a6e8b
Eat._.Noey <155715976+Jelliiiii@users.noreply.github.com>
2024-09-25 13:14:14 +0900

    Title: 규칙을 찾아 출력하기, Time: 135ms, Memory: 11MB, Status: Wrong Answer - CodeTree

--- 240925/규칙을 찾아 출력하기/find-a-rule-and-print.java
+++ 240925/규칙을 찾아 출력하기/find-a-rule-and-print.java
@@ -5,9 +5,8 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        // 전체 n개의 줄을 처리
+        // 첫 번째 줄과 마지막 줄은 항상 별로 가득 채움
         for (int i = 0; i < n; i++) {
-            
             // 첫 번째 줄과 마지막 줄
             if (i == 0 || i == n - 1) {
                 for (int j = 0; j < n; j++) {
@@ -17,20 +16,27 @@ public class Main {
                     }
                 }
             } 
-            // 중간 줄들 (첫 번째와 마지막 별 사이에 공백 처리)
+            // 중간 줄 처리
             else {
                 for (int j = 0; j < n; j++) {
-                    if (j == 0 || j == n - 1 || j < n - i) {
+                    if (j == 0 || j == n - 1) {
                         System.out.print("*");
-                    } else {
+                    } 
+                    // 중간의 공백 처리
+                    else if (j < i) {
                         System.out.print(" ");
+                    } 
+                    // 별 출력
+                    else if (j >= i) {
+                        System.out.print("*");
                     }
+
                     if (j < n - 1) {
                         System.out.print(" ");
                     }
                 }
             }
-            System.out.println(); // 줄 바꿈
+            System.out.println();
         }
         sc.close();
     }
