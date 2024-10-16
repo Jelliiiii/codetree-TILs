d87d6a1b78477b08c24d1d578256c7bc27800cf3
Eat._.Noey <155715976+Jelliiiii@users.noreply.github.com>
2024-10-16 11:16:28 +0900

    Title: 좌우 지그재그 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241016/좌우 지그재그 출력/print-lr-zigzag.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int startNum = 1;
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) { 
+                for (int j = 0; j < n; j++) {
+                    System.out.print(startNum + " ");
+                    startNum++;
+                }
+            } else { 
+                int endNum = startNum + n - 1;
+                for (int j = 0; j < n; j++) {
+                    System.out.print(endNum + " ");
+                    endNum--;
+                    startNum++;
+                }
+            }
+            System.out.println(); 
+
+        sc.close();
+    }
+}
