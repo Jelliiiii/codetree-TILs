a42225e2fbb57b4901657206c4c8ca54ff02ef25
Eat._.Noey <155715976+Jelliiiii@users.noreply.github.com>
2024-10-24 13:45:52 +0900

    Title: 구구단 만들기, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241024/구구단 만들기/print-multiplication-table.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for(int i=1; i<=9; i++){
+            for(int j=b; j>=a; j--){
+                if(j%2==0){
+                    System.out.print(j + " * " + i + " = " + (j*i));
+                    if(j>a){
+                        System.out.print(" / ");
+                    }
+                }
+                
+            }
+            System.out.println();
+        }
+
+        sc.close();
+        // 여기에 코드를 작성해주세요.
+    }
+}
