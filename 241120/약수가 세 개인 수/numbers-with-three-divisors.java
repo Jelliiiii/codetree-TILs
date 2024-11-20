4b2675e91bbc15eeff25e6c8aca2090c7f11270d
Eat._.Noey <155715976+Jelliiiii@users.noreply.github.com>
2024-11-20 19:40:58 +0900

    Title: 약수가 세 개인 수, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241120/약수가 세 개인 수/numbers-with-three-divisors.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+        int yak = 0; 
+
+        for (int i = start; i <= end; i++) { 
+            int cnt = 0; 
+            for (int j = 1; j * j <= i; j++) { 
+                if (i % j == 0) {
+                    cnt++;
+                    if (j != i / j) cnt++; 
+                }
+            }
+            if (cnt == 3) {
+                yak++;
+            }
+        }
+
+        System.out.print(yak);
+        sc.close();
+    }
+}
