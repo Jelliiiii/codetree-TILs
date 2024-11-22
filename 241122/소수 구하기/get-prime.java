21b0f079b1d73cefdf4cf102c91ee4c8e3729abe
Eat._.Noey <155715976+Jelliiiii@users.noreply.github.com>
2024-11-22 13:51:16 +0900

    Title: 소수 구하기, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 241122/소수 구하기/get-prime.java
+++ 241122/소수 구하기/get-prime.java
@@ -1,20 +1,23 @@
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i=1; i<=n; i++){
+
+        for (int i = 2; i <= n; i++) { // 1은 소수가 아니므로 2부터 시작
             int cnt = 0;
-            for(int j=1; j<=i; j++){
-                if(i%j==0){
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
                     cnt++;
                 }
-                if(cnt==2){
-                    System.out.print(i + " ");
-                }
-            }     
+            }
+            // 모든 약수를 확인한 후 cnt == 2인지 확인
+            if (cnt == 2) {
+                System.out.print(i + " ");
+            }
         }
+
         sc.close();
-        // 여기에 코드를 작성해주세요.
     }
 }
