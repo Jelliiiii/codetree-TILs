3fd442eabf6e484be11dae951ce7391d1dca1991
Eat._.Noey <155715976+Jelliiiii@users.noreply.github.com>
2024-12-11 18:42:52 +0900

    Title: 특정 숫자 도달하기, Time: 128ms, Memory: 10MB, Status: Runtime Error - CodeTree

--- 241211/특정 숫자 도달하기/reaching-specific-number.java
+++ 241211/특정 숫자 도달하기/reaching-specific-number.java
@@ -4,30 +4,25 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int sum = 0;
         int count = 0;
-        boolean oo = false;
 
-        for(int i=0; i<10; i++){
+        for (int i = 0; i < 10; i++) {
             int n = sc.nextInt();
-            if(n>=250){
-                oo = true;
-                break;
+            if (n >= 250) {
+                break; 
             }
             sum += n;
             count++;
         }
 
-        if(!oo){
-            for(int i=0; i<10; i++){
-                sum += sc.nextInt();
-                count++;
-            }
+        while (count < 10) {
+            int n = sc.nextInt();
+            sum += n;
+            count++;
         }
 
-        double avg = (double)sum/count;
-
+        double avg = (double) sum / count;
         System.out.printf("%d %.1f\n", sum, avg);
 
         sc.close();
-        // 여기에 코드를 작성해주세요.
     }
 }
