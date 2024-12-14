a8f109586a97b955bd8095851809d5fdb0eae9e8
Eat._.Noey <155715976+Jelliiiii@users.noreply.github.com>
2024-12-14 12:59:48 +0900

    Title: 평균점수 구하기, Time: 152ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 241214/평균점수 구하기/print-average.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double sum = 0;
+        for(int i=0; i<8; i++){
+            double score = sc.nextDouble();
+            sum += score;
+        }
+        double avg = sum/8;
+        System.out.printf("%.1f\n", avg);
+        sc.close();
+        // 여기에 코드를 작성해주세요.
+    }
+}
